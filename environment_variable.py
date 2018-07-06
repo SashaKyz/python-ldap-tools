739d6301b682d870c0243e1769cea4c44b104b04
Oleksandr Kuznetsov <Alex.Kuznetsov@osi.ca.gov>
2018-07-05 17:09:35 -0700

    Create script to get environment variable

--- environment_variable.py
+++ environment_variable.py
@@ -6,9 +6,11 @@ import re
 for root, dirs, files in os.walk(".."):
     for file in files:
         if ((file.find("docker-compose")!=-1) and (file.endswith(".yml"))):
-            print("Scan file: ",os.path.join(root, file))
-            shakes = open(os.path.join(root, file), "r")
-            for line in shakes:
-                sub_line=re.match("^\s*-\s(\w*)=.*",line)
-                if sub_line:
-                    print (sub_line.group(1))
+            application=re.match("roles/([\w,-]*)",os.path.join(root, file))
+            if application:
+                print("Environment variable for: ",application.group(1))
+                shakes = open(os.path.join(root, file), "r")
+                for line in shakes:
+                    sub_line=re.match("^\s*-\s(\w*)=.*",line)
+                    if sub_line:
+                        print (sub_line.group(1))
